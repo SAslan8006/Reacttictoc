669f9da8015435a53efa0f96ba60821edbf3788d
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-10-16 15:24:55 +0300

    Düzenleme

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,49 @@
+import { useState } from 'react'
 import './App.css'
 
 const Game = () => {
   return (
-    <div className='game'>
+    <div className="game">
       <Board />
     </div >
   )
 }
 
 const Board = () => {
+  const [values, setValues] = useState(Array(9).fill(""));
+  const [current, setCurrent] = useState("O");
+
+  const setSpecificValue = (index, value) => {
+    values[index] = value;
+    setValues((values) => [...values])
+  }
   return (
-    <div className='board' >
-      <Box />
-      <Box />
-      <Box />
-      <Box />
-      <Box />
-      <Box />
-      <Box />
-      <Box />
-      <Box />
+    <div className="board" >
+      {values.map((item, i) => {
+        <Box
+          key={i}
+          index={i}
+          value={values[i]}
+          current={current}
+          setCurrent={setCurrent}
+          setSpecificValue={setSpecificValue} />
+      })}
     </div>
   )
 }
-const Box = () => {
+const Box = ({ index, value, current, setCurrent, setSpecificValue }) => {
+  const ChangeCurrent = () => {
+    current === "O" ? setCurrent("X") : setCurrent("O")
+
+
+  }
   return (
-    <div className='box'>
-    </div>
+    <div className="box" onClick={() => {
+      if (current === "") {
+        setSpecificValue(index, current)
+        ChangeCurrent();
+      }
+    }}>{value}</div>
   )
 }
 
