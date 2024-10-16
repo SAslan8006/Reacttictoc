5f6a6556b85006a929214b5d9b20797dd4437a6f
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-10-16 16:01:39 +0300

    Tictacto Son Hali

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,103 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { winningCombinations } from './wc';  // Kazanma kombinasyonları dosyasını ekleyin
+
+const isGameOver = (values) => {
+  let gameOver = false;
+  let winner = "";
+  winningCombinations.forEach((wc) => {
+    let current = values[wc[0]];
+    let counter = 0;
+
+    wc.forEach((i) => {
+      if (values[i] !== "" && values[i] === current) counter++;
+      if (counter === 3) {
+        gameOver = true;
+        winner = current;
+        return;
+      }
+    });
+    if (gameOver) return;
+  });
+
+  return { winner, gameOver };
+};
 
 const Game = () => {
   return (
     <div className="game">
       <Board />
-    </div >
-  )
-}
+    </div>
+  );
+};
 
 const Board = () => {
-  const [values, setValues] = useState(Array(9).fill(""));
+  const ResetBoard = () => {
+    return Array(9).fill("");
+  };
+  const [values, setValues] = useState(ResetBoard());
   const [current, setCurrent] = useState("O");
 
   const setSpecificValue = (index, value) => {
     values[index] = value;
-    setValues((values) => [...values])
-  }
+    setValues([...values]);
+
+    const { winner, gameOver } = isGameOver(values);
+    if (gameOver) {
+      alert(`${winner} kazandı`);
+    } else {
+      console.log("Oyun Devam Ediyor");
+    }
+  };
+
   return (
-    <div className="board" >
-      {values.map((item, i) => {
-        <Box
-          key={i}
-          index={i}
-          value={values[i]}
-          current={current}
-          setCurrent={setCurrent}
-          setSpecificValue={setSpecificValue} />
-      })}
+    <div>
+      <button style={{ marginBottom: "50px" }} onClick={() => setValues(ResetBoard())}>
+        Restart
+      </button>
+      <div className="board">
+        {values.map((item, i) => (
+          <Box
+            key={i}
+            index={i}
+            value={values[i]}
+            current={current}
+            setCurrent={setCurrent}
+            setSpecificValue={setSpecificValue}
+          />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
+
 const Box = ({ index, value, current, setCurrent, setSpecificValue }) => {
   const ChangeCurrent = () => {
-    current === "O" ? setCurrent("X") : setCurrent("O")
+    if (current === "O") setCurrent("X");
+    else setCurrent("O");
+  };
 
-
-  }
   return (
-    <div className="box" onClick={() => {
-      if (current === "") {
-        setSpecificValue(index, current)
-        ChangeCurrent();
-      }
-    }}>{value}</div>
-  )
-}
-
+    <div
+      className="box"
+      onClick={() => {
+        if (value === "") {
+          setSpecificValue(index, current);
+          ChangeCurrent();
+        }
+      }}
+    >
+      {value}
+    </div>
+  );
+};
 
 function App() {
-
   return (
     <>
       <Game />
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
