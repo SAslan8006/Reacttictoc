fb6501778aaeec8313b433b011a96d8653b9d5b1
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-10-16 14:41:28 +0300

    Düzeltme

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,34 @@ import './App.css'
 
 const Game = () => {
   return (
-    <div style={{ background: "lightblue" }}>
-      Game
+    <div className='game'>
+      <Board />
+    </div >
+  )
+}
+
+const Board = () => {
+  return (
+    <div className='board' style={{ background: "lightgreen", display: "flex", flexWrap: "wrap", width: "306px" }}>
+      <Box />
+      <Box />
+      <Box />
+      <Box />
+      <Box />
+      <Box />
+      <Box />
+      <Box />
+      <Box />
     </div>
   )
 }
+const Box = () => {
+  return (
+    <div style={{ background: "lightgrey", width: "100px", height: "100px", border: "1px solid black" }}>
+    </div>
+  )
+}
+
 
 function App() {
 
